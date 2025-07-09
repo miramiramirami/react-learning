94a3e30447d27c9fbd6a8754bf79cf991286ea7a
miramiramirami <xeless@mail.ru>
2025-07-09 19:37:36 +0500

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header"
+import Header from "./components/Header/Header"
 import IntroSection from "./components/IntroSection"
 import RulesSection from './components/RulesSection'
 import TabsSection from "./components/TabsSection"
